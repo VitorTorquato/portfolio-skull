088fdee93b3776206a5280706711177444b04c1b
Vitor <torquatovitor68@gmail.com>
2024-02-16 08:58:30 +0100

    finished

--- src/main.js
+++ src/main.js
@@ -1,3 +1,7 @@
+const btnSubmit = document.getElementById('btnSubmit');
+const btnHamburguer = document.querySelector('.hamburguer');
+//Elements
+
 window.onload = function () {
     window.addEventListener("scroll", function (e) {
         if (window.scrollY > 100) {
@@ -6,4 +10,18 @@ window.onload = function () {
             document.querySelector('header').classList.remove('is-scrolling');
         }
     });
+    
+    btnHamburguer.addEventListener('click' ,(e) => {
+        
+        e.preventDefault();
+        
+        btnHamburguer.classList.toggle('is-active');
+        document.querySelector('.mobile-nav').classList.toggle('is-active')
+        
+    })
+
+    btnSubmit.addEventListener('click' , (e) => {
+        e.preventDefault();
+    })
 };
+
